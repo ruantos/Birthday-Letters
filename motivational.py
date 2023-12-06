4e309aea8dc56df8171f9eeb29eb2868803b6385
Juan Oliveira <juanoliveira@pop-os.localdomain>
2023-12-06 17:54:28 -0300

    motivational email

---
+++ motivational.py
@@ -0,0 +1,31 @@
+import smtplib
+import random as rd
+import datetime as dt
+
+email = "robson.juan@ufrpe.br"
+password = "frzaujrypyrqesvg"
+
+def check_date():
+    date = dt.datetime.now()
+    weekday = date.weekday()
+    if weekday == 2:
+        message = get_quote()
+        send_email(message)
+
+def get_quote():
+    with open("quotes.txt", mode="r") as file:
+        quotes_list = file.readlines()
+        quote = rd.choice(quotes_list)
+    subject = "Subject:Happy Birthday, Juan\n\n"
+    message = subject + quote
+    return message
+    
+def send_email(message):
+    global email, password
+    with smtplib.SMTP("smtp.gmail.com", 587) as connection:
+        connection.starttls()
+        connection.login(user=email, password=password)
+        connection.sendmail(from_addr=email, to_addrs=email, msg=message)
+        connection.close()
+
+check_date()
